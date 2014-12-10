abfe5e5bd6f634d87992d1267c97959a238d4a37
garyzhang2681 <garyzhang2681@gmail.com>
2014-12-10 22:30:11 +0800

    1210

--- src/Sort/FunctionTest.java
+++ src/Sort/FunctionTest.java
@@ -8,16 +8,13 @@ public class FunctionTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//2,9,1,4,4,5,5,6,7,2,3,
-		int[] array = {2,9,1,4,4,5,5,6,7,2,3};
+		int[] array = {3,4,2,4};
 		//int[] array = {7,6,9};
 		
-		ArrayList<Integer> x = new ArrayList<>();
+	
 		
-		
-		Collections.sort(x);
-		
-		//Sort s = new QuictSort();
-		Sort s = new SelectionSort();
+		Sort s = new QuickSort();
+		//Sort s = new SelectionSort();
 		s.sort(array);
 		
 		
---
+++ src/Sort/QuickSort.java
@@ -0,0 +1,108 @@
+package Sort;
+
+import java.util.ArrayList;
+
+public class QuickSort implements Sort {
+
+	// public static void QuickSort1(int[] a, int lo0, int hi0) {
+	// int lo = lo0;
+	// int hi = hi0;
+	//
+	// if (lo >= hi)
+	// return;
+	//
+	// // 确定指针方向的逻辑变量
+	// boolean transfer = true;
+	//
+	// while (lo != hi) {
+	// if (a[lo] > a[hi]) {
+	// // 交换数字
+	// int temp = a[lo];
+	// a[lo] = a[hi];
+	// a[hi] = temp;
+	// // 决定下标移动，还是上标移动
+	// transfer = (transfer == true) ? false : true;
+	// }
+	//
+	// // 将指针向前或者向后移动
+	// if (transfer)
+	// hi--;
+	// else
+	// lo++;
+	//
+	// // 显示每一次指针移动的数组数字的变化
+	//
+	// for(int i = 0; i < a.length; ++i) { System.out.print(a[i] + ",");
+	// } System.out.print(" (lo,hi) = " + "(" + lo + "," + hi + ")");
+	// System.out.println("");
+	//
+	// }
+	//
+	// // 将数组分开两半，确定每个数字的正确位置
+	// lo--;
+	// hi++;
+	// QuickSort(a, lo0, lo);
+	// QuickSort(a, hi, hi0);
+	// }
+
+	public void quickSort(int[] array, int startIndex, int endIndex) {
+
+		CommonFunctionality.printPartArray(array, startIndex, endIndex);
+		if (startIndex >= endIndex) {
+			return;
+		}
+		int i = startIndex;
+		int j = endIndex;
+
+		int middleNumber = array[(startIndex + endIndex) / 2];
+
+		
+		
+		
+		array[(startIndex + endIndex) / 2] = array[startIndex];
+		while (i < j) {
+
+			while (array[j] >= middleNumber && i < j) {  //等于号一定要放在>=上
+				j--;
+			}
+			array[i] = array[j];
+			
+			while (array[i] < middleNumber && i < j) { 
+				i++;
+			}
+			array[j] = array[i];
+			CommonFunctionality.printArray(array);
+		}
+		
+		
+		
+		
+//		array[(startIndex + endIndex) / 2] = array[endIndex];
+//		while (i < j) {
+//
+//			while (array[i] < middleNumber && i < j) { 
+//				i++;
+//			}
+//			array[j] = array[i];
+//			
+//			while (array[j] >= middleNumber && i < j) {  //等于号一定要放在>=上
+//				j--;
+//			}
+//			array[i] = array[j];
+//			CommonFunctionality.printArray(array);
+//
+//		}
+		
+		
+		array[i] = middleNumber;
+		quickSort(array, startIndex, i);
+		quickSort(array, i + 1, endIndex);
+		CommonFunctionality.printArray(array);
+	}
+
+	@Override
+	public void sort(int[] unsortedArray) {
+		quickSort(unsortedArray, 0, unsortedArray.length - 1);
+	}
+
+}
