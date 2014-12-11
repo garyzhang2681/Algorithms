4de26200f5ea6378594eacd3e6402fa20a2247c8
garyzhang2681 <garyzhang2681@gmail.com>
2014-12-11 17:59:23 +0800

    1211

--- src/Sort/InsertionSort.java
+++ src/Sort/InsertionSort.java
@@ -2,11 +2,51 @@ package Sort;
 
 public class InsertionSort implements Sort {
 
-	public void InsertionSort(int [] unsortedArray)
-	{
+	public void InsertionSort(int[] array) {
 		
+		for (int i = 1; i < array.length; i++) {
+			CommonFunctionality.printArray(array);
+			int temp = array[i];
+			int j = i;
+			
+			
+			//方法1
+			while (j>0 && temp < array[j-1]) {
+					array[j] = array[j - 1];
+					j--;
+			}
+			array[j] = temp;
+			CommonFunctionality.printArray(array);
+			
+			//方法2	
+//			while (j>0) {
+//				if (temp < array[j-1]) {
+//					array[j] = array[j - 1];
+//					j--;
+//				} else{
+//					break;
+//				}
+//			}
+//			array[j] = temp;
+//			CommonFunctionality.printArray(array);
+			
+			
+			
+			//方法3
+//			for (int j = i; j > 0; j--) {
+//				if (temp < array[j-1]) {
+//					array[j] = array[j - 1];
+//					if(j == 1){
+//						array[0] = temp;
+//					}
+//				} else {
+//					array[j] = temp;
+//					break;
+//				}
+//			}
+		}
 	}
-	
+
 	@Override
 	public void sort(int[] unsortedArray) {
 		// TODO Auto-generated method stub
---
+++ src/Sort/ShellSort.java
@@ -0,0 +1,11 @@
+package Sort;
+
+public class ShellSort implements Sort {
+
+	@Override
+	public void sort(int[] unsortedArray) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
